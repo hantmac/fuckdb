45f15d987be51d84ec83f65387fe0d5cd6bfa205
hantmac <hantmac@outlook.com>
2019-12-12 15:13:19 +0800

    fix

--- routers/download.go
+++ routers/download.go
@@ -35,14 +35,15 @@ func DownloadStructFile(c *gin.Context) {
 	var filename = filepath.Join(viper.GetString("genFilePath"), downLoadInfo.StructName) + ".go"
 	if !utils.CheckFileIsExist(filename) {
 		file, err := os.Create(filename) //创建文件
+		defer file.Close()
 		if err != nil {
-			c.String(400, err.Error())
+			services.HandleError(http.StatusInternalServerError, c, err)
 			return
 		}
 		buf := bufio.NewWriter(file) //创建新的 Writer 对象
 		buf.WriteString(downLoadInfo.Content)
 		buf.Flush()
-		defer file.Close()
+
 	}
 	//返回文件路径
 	c.JSON(http.StatusOK, services.Response{
