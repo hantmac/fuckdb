9990124c4d95b761f7b7b5f34dbb25a7c33a697b
hantmac <hantmac@outlook.com>
2020-01-09 15:40:48 +0800

    add yaml to json

--- frontend/src/components/home.vue
+++ frontend/src/components/home.vue
@@ -9,6 +9,7 @@
             <el-dropdown-menu slot="dropdown">
               <el-dropdown-item command="FuckDb">FuckDb</el-dropdown-item>
               <el-dropdown-item command="json-to-go">json-to-go</el-dropdown-item>
+               <el-dropdown-item command="yaml-to-json">yaml-to-json</el-dropdown-item>
               <!-- <el-dropdown-item command="struct-to-sql">struct-to-sql</el-dropdown-item> -->
             </el-dropdown-menu>
         </el-dropdown>
@@ -126,6 +127,9 @@
     </div>
    <div class="m-tool" v-show="tabName == 'json-to-go'">
       <iframe src="https://mholt.github.io/json-to-go/" frameborder="0"></iframe>
+    </div>
+    <div class="m-tool" v-show="tabName == 'yaml-to-json'">
+      <iframe src="https://onlineyamltools.com/convert-yaml-to-json" frameborder="0"></iframe>
     </div>
     <div class="" v-show="tabName == 'struct-to-sql'">
       <el-tabs type="border-card">
